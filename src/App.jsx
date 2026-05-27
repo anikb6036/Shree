61c28c01cbc09784c19ab69170a7e4a90316e384
Anik <anik.baidya@hotmail.com>
2026-05-27 14:56:36 +0530

    Add additional images and update image source in MidnightEditorialHomePage component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './index.css'; // Plain CSS, not module
 import image4 from './assets/image4.jpg';
 import image3 from './assets/image3.jpg';
-
+import image2 from './assets/image2.jpg';
+import image1 from './assets/image1.jpg';
 
 const MidnightEditorialHomePage = () => {
   return (
@@ -92,7 +93,7 @@ const MidnightEditorialHomePage = () => {
         </div>
         <div className="mh-gallery-item">
           <div className="mh-gallery-overlay" />
-          <img src='src/assets/image2.jpg' alt="Foggy forest" className="mh-gallery-img" />
+          <img src='https://scontent.fccu14-1.fna.fbcdn.net/v/t39.30808-6/481105707_1666993454193045_8283542299008702679_n.jpg?stp=c236.0.608.608a_dst-jpg_s206x206_tt6&_nc_cat=111&ccb=1-7&_nc_sid=714c7a&_nc_ohc=fkNvSn5a8GUQ7kNvwEGQEYS&_nc_oc=AdrIUl_qDpJFuAAXMNtzFuTw_ioghS9kNKsFrm7zCjJjmhkq5HBM4xnzwuLLOaA4gIc&_nc_zt=23&_nc_ht=scontent.fccu14-1.fna&_nc_gid=XzOOyi17kJwNueGuSrjhTg&_nc_ss=7b2a8&oh=00_Af5zvRSvFb31WQacallguqcwjbieY97xIlTaqVVPKgHOCA&oe=6A1CA47F' alt="Foggy forest" className="mh-gallery-img" />
           <span className="mh-gallery-caption">bablu · 2024</span>
         </div>
       </div>
