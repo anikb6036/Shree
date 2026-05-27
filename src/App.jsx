cc6364a9de9c2f5a8a5da7a64952bd44cfc33b31
Anik <anik.baidya@hotmail.com>
2026-05-27 14:42:31 +0530

    Add images and update background images in MidnightEditorialHomePage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './index.css'; // Plain CSS, not module
+import image4 from './assets/image4.jpg';
+import image3 from './assets/image3.jpg';
+
 
 const MidnightEditorialHomePage = () => {
   return (
@@ -23,7 +26,7 @@ const MidnightEditorialHomePage = () => {
         <div
           className="mh-hero-image"
           style={{
-            backgroundImage: 'url(https://scontent.fccu14-1.fna.fbcdn.net/v/t39.30808-6/481234384_1666995387526185_8736406767444040515_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Dvshk-Dq6m0Q7kNvwFx0iK_&_nc_oc=AdqOfzhLfgx2Uf2PcX3aYwNVhXpnRBWzRCbCELgmFtvCuO-yf32GU146V2wKGJB63HY&_nc_zt=23&_nc_ht=scontent.fccu14-1.fna&_nc_gid=mtJfVhFRMYBU0H20sPA26g&_nc_ss=7b2a8&oh=00_Af7W_0n_5B07x7qBzpnWU4el2ZonbYuOamGpg40lsX0dRw&oe=6A1C8FD2)',
+            backgroundImage: `url(${image4})`,
           }}
         />
         <h1 className="mh-hero-title">Dark<br />Matter</h1>
@@ -65,7 +68,7 @@ const MidnightEditorialHomePage = () => {
       {/* Full-width image */}
       <div
         className="mh-full-image"
-        style={{ backgroundImage: 'url(https://scontent-ccu1-1.xx.fbcdn.net/v/t39.30808-6/481282540_1668391330719924_1555992137457040272_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_ohc=dPhXTHyFc7wQ7kNvwHqst7T&_nc_oc=Adq7MouMPhy9PAUD7ZGysTbzM9Gw2HYaIJR6PSDKUPMo4dB3TLY5OcdyBujzj-lMs6c&_nc_zt=23&_nc_ht=scontent-ccu1-1.xx&_nc_gid=gKleC_YZ5tKey04P3BGN5w&_nc_ss=7b2a8&oh=00_Af57DzTjnNuxcw9gi0h5M5v_bhqjIPTXlc9xQRye1uZkew&oe=6A138E21)' }}
+        style={{ backgroundImage: `url(${image3})` }}
         role="img"
         aria-label="Gallery image"
       />
@@ -84,13 +87,13 @@ const MidnightEditorialHomePage = () => {
       <div className="mh-side-gallery">
         <div className="mh-gallery-item">
           <div className="mh-gallery-overlay" />
-          <img src="https://scontent-ccu2-2.xx.fbcdn.net/v/t39.30808-6/481821247_1672134727012251_7823841779385785307_n.jpg?stp=c151.0.778.778a_dst-jpg_s206x206_tt6&_nc_cat=107&ccb=1-7&_nc_sid=714c7a&_nc_ohc=iK-k7M8YHEsQ7kNvwGS4AYH&_nc_oc=AdqcZsCaxfiTV3X7iXlbAeT_1TzqEyHfa1Vc3cKqKED1H5jSQ6rs4PdswUTATbMD8KI&_nc_zt=23&_nc_ht=scontent-ccu2-2.xx&_nc_gid=QFQHdGRQbVMGcmKo77x6ag&_nc_ss=7b2a8&oh=00_Af5bFrl87OTYeyTf8awilLZG86fgfwkUmJNht7J6BYjhgA&oe=6A1390F8" alt="Mountain" className="mh-gallery-img" />
+          <img src='src/assets/image1.jpg' alt="Mountain" className="mh-gallery-img" />
           <span className="mh-gallery-caption">anik-shree · 2025</span>
         </div>
         <div className="mh-gallery-item">
           <div className="mh-gallery-overlay" />
-          <img src="https://scontent-ccu1-2.xx.fbcdn.net/v/t39.30808-6/481105707_1666993454193045_8283542299008702679_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_ohc=Hbjituletc8Q7kNvwHzuhhz&_nc_oc=AdpGIeDnRc-Yh4fNMRI47-SUFxoo4Pw-zwCnFMJIwA_GXawBAG_0SoqBpw6ispqNsCw&_nc_zt=23&_nc_ht=scontent-ccu1-2.xx&_nc_gid=tYfVG_wPKUEGDY_VrVSncw&_nc_ss=7b2a8&oh=00_Af6UCT7Ch6_4AXH1RpBaVwnvcjnvK0ack9pt8jSZzgOoxg&oe=6A13A23F" alt="Foggy forest" className="mh-gallery-img" />
-          <span className="mh-gallery-caption">bablu · 2026</span>
+          <img src='src/assets/image2.jpg' alt="Foggy forest" className="mh-gallery-img" />
+          <span className="mh-gallery-caption">bablu · 2024</span>
         </div>
       </div>
 
