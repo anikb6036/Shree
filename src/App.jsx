aa59f64acbb074baed0d5e4fa1fdaf790fb61619
Anik <anik.baidya@hotmail.com>
2026-05-27 15:00:49 +0530

    Refactor image sources in MidnightEditorialHomePage component to use variables

--- src/App.jsx
+++ src/App.jsx
@@ -88,12 +88,12 @@ const MidnightEditorialHomePage = () => {
       <div className="mh-side-gallery">
         <div className="mh-gallery-item">
           <div className="mh-gallery-overlay" />
-          <img src='src/assets/image1.jpg' alt="Mountain" className="mh-gallery-img" />
+          <img src={image1} alt="Mountain" className="mh-gallery-img" />
           <span className="mh-gallery-caption">anik-shree · 2025</span>
         </div>
         <div className="mh-gallery-item">
           <div className="mh-gallery-overlay" />
-          <img src='https://scontent.fccu14-1.fna.fbcdn.net/v/t39.30808-6/481105707_1666993454193045_8283542299008702679_n.jpg?stp=c236.0.608.608a_dst-jpg_s206x206_tt6&_nc_cat=111&ccb=1-7&_nc_sid=714c7a&_nc_ohc=fkNvSn5a8GUQ7kNvwEGQEYS&_nc_oc=AdrIUl_qDpJFuAAXMNtzFuTw_ioghS9kNKsFrm7zCjJjmhkq5HBM4xnzwuLLOaA4gIc&_nc_zt=23&_nc_ht=scontent.fccu14-1.fna&_nc_gid=XzOOyi17kJwNueGuSrjhTg&_nc_ss=7b2a8&oh=00_Af5zvRSvFb31WQacallguqcwjbieY97xIlTaqVVPKgHOCA&oe=6A1CA47F' alt="Foggy forest" className="mh-gallery-img" />
+          <img src={image2} alt="Foggy forest" className="mh-gallery-img" />
           <span className="mh-gallery-caption">bablu · 2024</span>
         </div>
       </div>
